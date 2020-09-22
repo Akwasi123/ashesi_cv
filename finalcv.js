4ec805d3066e2101e7e317a8442003ffa6c59ad8
Akwasi Appiadjei Asante-Krobea <49639685+Akwasi123@users.noreply.github.com>
2020-09-22 10:27:49 +0000

    Modified form.css

--- finalcv.js
+++ finalcv.js
@@ -2,6 +2,7 @@
  * @author Akwasi Appiadjei Asante-Krobea
  */
 
+ //creating variables to get the data from local storage
 firstName = localStorage.getItem("first");
 middleName = localStorage.getItem("middle");
 lastName = localStorage.getItem("last");
@@ -10,6 +11,7 @@ phoneNumber = localStorage.getItem("phone");
 email = localStorage.getItem("email");
 address = localStorage.getItem("address");
 
+//Appending data from local storage to their respective fields
 document.getElementById("name").innerHTML = firstName +" "+middleName+" "+lastName;
 document.getElementById("address").innerHTML = address;
 document.getElementById("phoneNumber").innerHTML = phoneNumber;
--- formvalidation.js
+++ formvalidation.js
@@ -1,9 +1,16 @@
+//accessing the form element from the DOM
 const form = document.getElementById("_form");
 
 
+/**
+ * function to validate input on Submission
+ * @param {*} e 
+ */
 function onSubmit(e){
+    //prevents the page from erasing the data when form is submitted
     e.preventDefault();
 
+    //creating variables for the form fields and setting up local storage for each value
     const fname = document.getElementById("first");
     localStorage.setItem("first", fname.value);
 
@@ -29,29 +36,35 @@ function onSubmit(e){
     const number_format = /1?-?\.?\(?\d{3}[\-\)\.\s]?\d{3}[\-\.\s]?\d{4}/;
     const email_format = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
+    //checking if name format is correct
     if(!name_format.test(fname.value)){
         alert("Enter first name");
         return false;
     }
 
+    //checking if name format is correct
     if(!name_format.test(lname.value)){
         alert("Enter last name");
         return false;
     }
 
+    //checking for correct phone number format
     if(!number_format.test(phoneNumber.value)){
         alert("Enter a valid Phone Number");
         return false;
     }
 
+    //checking for correct email format
     if(!email_format.test(email.value)){
         alert("Enter a valid email address.");
         return false;
     } 
     
+    //go to cv page once inputs have been validated
     else{
         location.href = "cv.html";
     }
 }
 
+//adds an event listner to the form
 form.addEventListener("submit", onSubmit);
